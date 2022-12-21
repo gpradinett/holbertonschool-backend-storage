d3979f763b59bd26c74d7a96746d9e90440fb5b4
gpradinett <gpradinett@gmail.com>
2022-12-20 23:54:24 -0500

    ✅ Python function that returns all students sorted by average score:

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Python function that returns all students sorted by average score:
+"""
+
+
+def top_students(mongo_collection):
+    """This function returns all students sorted by average score"""
+    doc = mongo_collection.aggregate([
+        { "$unwind": "$topics" },
+        { "$group": {
+            "_id": "$_id",
+            "name": { "$first": "$name" },
+            "averageScore": { "$avg": "$topics.score" }
+        }},
+        { "$sort": { "averageScore": -1 } }
+    ])
+    return list(doc)
