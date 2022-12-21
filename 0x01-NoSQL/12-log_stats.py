0e88c8cc7b9490db1c2d8a7cdc72fd9a0693868e
gpradinett <gpradinett@gmail.com>
2022-12-20 23:38:09 -0500

    ✅ Python script that provides some stats about Nginx logs stored in MongoDB:

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,30 +4,23 @@ from pymongo import MongoClient
  Python script that provides some stats about Nginx logs stored in MongoDB:
 """
 
+from pymongo import MongoClient
+
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
-
-    number_logs = collection.count_documents({})
-    print("""{} logs
-Methods:""".format(number_logs))
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    for method in methods:
-        logs = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, logs))
-
-    pathlogs = collection.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(pathlogs))
-
-    print("IPs:")
-    ips = collection.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ])
-
-    for ip in ips:
-        print("\t{}: {}".format(ip["_id"], ip["count"]))
+    print("{} logs".format(collection.estimated_document_count()))
+    print("Methods:")
+    print("\tmethod GET: {}".format(collection.count_documents
+                                    ({"method": "GET"})))
+    print("\tmethod POST: {}".format(collection.count_documents
+                                     ({"method": "POST"})))
+    print("\tmethod PUT: {}".format(collection.count_documents
+                                    ({"method": "PUT"})))
+    print("\tmethod PATCH: {}".format(collection.count_documents
+                                      ({"method": "PATCH"})))
+    print("\tmethod DELETE: {}".format(collection.count_documents
+                                       ({"method": "DELETE"})))
+    print("{} status check".format(collection.count_documents
+                                   ({"path": "/status"})))
