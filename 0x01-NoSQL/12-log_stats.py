9423730f054933717e8bd1095f919b48bcc82536
gpradinett <gpradinett@gmail.com>
2022-12-20 21:27:20 -0500

    ✅ Python script that provides some stats about Nginx logs stored in MongoDB:

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import pymongo
+"""
+
+"""
+
+
+if __name__ == "__main__":
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["logs"]
+coll = db["nginx"]
+
+# Obtiene el número total de documentos en la colección
+total_count = coll.count_documents({})
+print(f"{total_count} logs")
+
+# Imprime el número de documentos para cada método HTTP
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+print("\tMethods:")
+for method in methods:
+    count = coll.count_documents({"method": method})
+    print(f"\t\t{count}\t{method}")
+
+# Imprime el número de documentos con método=GET y path=/status
+count = coll.count_documents({"method": "GET", "path": "/status"})
+print(f"\t\t{count}\tGET /status")
+
+
+
