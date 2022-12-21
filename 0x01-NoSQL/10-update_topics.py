eaf8316c92c67d8963bdb07ec3d067db2114953c
gpradinett <gpradinett@gmail.com>
2022-12-20 19:53:39 -0500

    ✅ Python function that changes all topics of a school document based on the name

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -13,4 +13,4 @@ def update_topics(mongo_collection, name, topics):
         {"name": name},
         {"$set": {"topics": "topics"}})
     """ Returns the number of updated documents """
-    return result.modified_count
+    return result
