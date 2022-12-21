e50f6db9a2e37a8d50c46d11eaf176ce6b7cc8b7
gpradinett <gpradinett@gmail.com>
2022-12-21 17:36:41 -0500

    ✅ Cache class. In the __init__ method

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import uuid
+from typing import Union
+import redis
+"""
+Writing strings to Redis
+"""
+
+
+class Cache:
+    """ Stores the history of inputs and outputs for a particular
+    function
+    """
+    def __init__(self):
+        """ Defining the constructor """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ method take a data and returns a string """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
