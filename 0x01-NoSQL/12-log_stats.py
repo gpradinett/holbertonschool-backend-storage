3cf78b7e51b6bed4047c812580fcf1eb98934cdf
gpradinett <gpradinett@gmail.com>
2022-12-20 23:46:14 -0500

    ✅ Python script that provides some stats about Nginx logs stored in MongoDB:

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,21 +8,18 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["logs"]
-    coll = db["nginx"]
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
 
-    # Obtiene el número total de documentos en la colección
-    total_count = coll.count_documents({})
-    print(f"{total_count} logs")
+    number_logs = collection.count_documents({})
+    print("""{} logs
+Methods:""".format(number_logs))
 
-    # Imprime el número de documentos para cada método HTTP
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("\tMethods:")
-    for method in methods:
-        count = coll.count_documents({"method": method})
-        print(f"\t\t{count}\t{method}")
 
-    # Imprime el número de documentos con método=GET y path=/status
-    count = coll.count_documents({"method": "GET", "path": "/status"})
-    print(f"\t\t{count}\tGET /status")
+    for method in methods:
+        logs = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, logs))
+
+    pathlogs = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(pathlogs))
